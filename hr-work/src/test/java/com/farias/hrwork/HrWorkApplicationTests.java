5a02f21c7fb66b07a1fe745c8d947a1aaf4b1b30
isacfarias <isac.farias.21@gmail.com>
2021-03-08 15:10:20 -0400

    Project hr-worker created

---
+++ hr-work/src/test/java/com/farias/hrwork/HrWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farias.hrwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
