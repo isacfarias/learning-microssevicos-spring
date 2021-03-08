e06defc500239dc8dd87a2724cb4291ca0d3c8c7
isacfarias <isac.farias.21@gmail.com>
2021-03-08 19:24:07 -0400

    removendo imposts desnecessarios

--- hr-payroll/src/main/java/com/farias/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/farias/hrpayroll/resources/PaymentResource.java
@@ -5,18 +5,15 @@ import com.farias.hrpayroll.service.PaymentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
-
-
 
     @Autowired
     private PaymentService service;
