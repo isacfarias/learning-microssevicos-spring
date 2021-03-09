c2d70ad8a4709d6537f4d6ecee74271156e11938
isacfarias <isac.farias.21@gmail.com>
2021-03-09 10:47:37 -0400

    Projeto hr-apigateway-zuul created

---
+++ hr-apigateway-zuul/src/test/java/com/farias/hapigatewayzuul/HrApigatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farias.hapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApigatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
