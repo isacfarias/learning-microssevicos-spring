3b17759fdef09d6fc989a2c8088733499f34c5aa
isacfarias <isac.farias.21@gmail.com>
2021-03-08 21:50:41 -0400

    add logger para visualizar o balanceamento de carga com ribbon

--- hr-work/src/main/java/com/farias/hrwork/resources/WorkResource.java
+++ hr-work/src/main/java/com/farias/hrwork/resources/WorkResource.java
@@ -5,7 +5,10 @@ import java.util.List;
 import com.farias.hrwork.entities.Worker;
 import com.farias.hrwork.repositories.WorkRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping( value = "/workers")
 public class WorkResource {
+
+    private static Logger log = LoggerFactory.getLogger(WorkResource.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkRepository repository;
@@ -31,11 +39,9 @@ public class WorkResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> worker(@PathVariable Long id) {
+        log.info("PORT: "+ env.getProperty("local.server.port"));
 
         Worker worker = repository.findById(id).get(); 
-
         return ResponseEntity.ok(worker);
     }
-
-    
 }
