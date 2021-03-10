a490813892801696628caaae81b5511f8a7f3173
isacfarias <isac.farias.21@gmail.com>
2021-03-09 21:20:39 -0400

    private git repository

--- hr-config-server/src/main/java/com/farias/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/farias/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,27 @@
 package com.farias.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner{
+
+
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("CONFIG: "+ env.getProperty("spring.cloud.config.server.git.username"));
 	}
 
 }
