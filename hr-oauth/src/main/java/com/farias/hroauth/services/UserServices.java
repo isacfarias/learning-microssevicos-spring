18c60692eb269d2369190dc9b0ef656a6e42745c
isacfarias <isac.farias.21@gmail.com>
2021-03-11 10:07:35 -0400

    login, JWT token

--- hr-oauth/src/main/java/com/farias/hroauth/services/UserServices.java
+++ hr-oauth/src/main/java/com/farias/hroauth/services/UserServices.java
@@ -6,10 +6,13 @@ import com.farias.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServices {
+public class UserServices implements UserDetailsService {
 
     private static Logger log = LoggerFactory.getLogger(UserServices.class);
 
@@ -20,11 +23,14 @@ public class UserServices {
         User user = userFeignClient.email(email).getBody();
         if (user == null) {
             log.error("Email not found:" + email);
-            throw new IllegalArgumentException("Email not found");
+            throw new UsernameNotFoundException("Email not found");
         }
         log.info("Email found" + user.getEmail());
         return user;
-
     }
 
+    @Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return findByEmail(username);
+	}
 }
