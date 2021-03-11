e23e53a3b6c54362e5d957f6de4ed277858c8469
isacfarias <isac.farias.21@gmail.com>
2021-03-11 14:43:11 -0400

    Security configs to config server

--- hr-apigateway-zuul/src/main/java/com/farias/hapigatewayzuul/config/AppConfig.java
+++ hr-apigateway-zuul/src/main/java/com/farias/hapigatewayzuul/config/AppConfig.java
@@ -1,17 +1,23 @@
 package com.farias.hapigatewayzuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 public class AppConfig {
+	
+	@Value(value = "${jwt.secret}")
+	private String secret;
 	
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("MY-SECRET-KEY");
+        converter.setSigningKey(secret);
         return converter;
     }
 
@@ -19,6 +25,5 @@ public class AppConfig {
     public JwtTokenStore tokenStore() {
         return new JwtTokenStore(accessTokenConverter());
     }
-    
 
 }
--- hr-oauth/src/main/java/com/farias/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/farias/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
  package com.farias.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value(value = "${jwt.secret}")
+	private String sceret;
+	
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -17,7 +21,7 @@ public class AppConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("MY-SECRET-KEY");
+        converter.setSigningKey(sceret);
         return converter;
     }
 
